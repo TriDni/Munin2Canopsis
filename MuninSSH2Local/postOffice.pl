cb78cc278ddfc2f7546dbdca311aa565eacc709d
TriD <tristan.denni@viacesi.fr>
2013-07-24 09:50:18 +0200

    Update postOffice.pl

--- MuninSSH2Local/postOffice.pl
+++ MuninSSH2Local/postOffice.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl
 
+# License
+#   GPLv2 (http://www.gnu.org/licenses/gpl-2.0.txt)
+# Authors
+#   DENNI Tristan
+
 use strict;
-use warnings;
 use Data::Dumper;
 use JSYNC;
 use Net::RabbitFoot;
